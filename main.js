bd8dafaa6584930c439305d43be1d02c2f5b8e8f
Brad Chapin <paledoor@yahoo.com>
2018-12-05 17:01:26 -0500

    working function, inelegant but accurate

---
+++ main.js
@@ -0,0 +1,20 @@
+function sortArray(array) {
+let odds = [];
+if (!array) {
+  return array;
+}
+for (let i = 0; i < array.length; i++) {
+  if (array[i] % 2 != 0) {
+    odds.push(array[i]);
+    array[i] = "x";
+  }
+}
+odds = odds.sort(function (a,b){
+  return (a-b);
+});
+for (let k = 0; k <odds.length; k++) {
+  let n = array.indexOf("x");
+  array[n] = odds[k];
+}
+return array;
+}
